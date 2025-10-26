e4b9e01a5f4f4fa5516afc8829a761a580ece36d
Soumyajit <4soumya19@gmail.com>
2025-10-26 22:13:07 +0530

    deploy

--- segment/vite.config.js
+++ segment/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Assign/segment/",
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://webhook.site/2be76ee8-ea2b-42e2-a3b2-f7cae73d82df',
+      "/api": {
+        target: "https://webhook.site/2be76ee8-ea2b-42e2-a3b2-f7cae73d82df",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-})
+});
